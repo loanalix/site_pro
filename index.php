7576d152b2acb9dde4f587ca720cff5b4f9112c0
loanalix <117281608+loanalix@users.noreply.github.com>
2023-01-23 11:40:06 +0100

    d
    Co-authored-by: Xiwang <xiwangoffi@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.mi.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+    <title>The Sense</title>
+</head>
+<body>
+    
+    <h1 class="red">
+        test
+    </h1>
+
+    <!-- Script -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    
+
+</body>
+</html>
